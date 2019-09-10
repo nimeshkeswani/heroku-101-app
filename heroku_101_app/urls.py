9f921348be6d9ad317f24818924c792c2f598a7f
Nimesh Keswani <nimesh@chewse.com>
2019-09-09 21:19:13 -0700

    add a Hello World! page

--- heroku_101_app/urls.py
+++ heroku_101_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from page_visits import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.hello_world),
 ]
