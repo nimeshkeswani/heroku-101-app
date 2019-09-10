9426915a4db9220c27800e3449739199ad595c0a
Nimesh Keswani <nimesh@chewse.com>
2019-09-09 22:12:39 -0700

    add settings to use Heroku Postgres, but SQLite on local

--- heroku_101_app/settings.py
+++ heroku_101_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +77,8 @@ WSGI_APPLICATION = 'heroku_101_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
@@ -125,4 +123,10 @@ STATICFILES_DIRS = [
     STATIC_ROOT,
 ]
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 django_heroku.settings(locals())
+
+del DATABASES['default']['OPTIONS']['sslmode']
