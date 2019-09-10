facc0b360ebda23e33a9e14d9a8f3cc2499e6f6f
Nimesh Keswani <nimesh@chewse.com>
2019-09-09 21:27:15 -0700

    create a model to store visitor counts

--- page_visits/models.py
+++ page_visits/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class PageVisits(models.Model):
+    count = models.IntegerField()
--- page_visits/views.py
+++ page_visits/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import PageVisits
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    page_visits = PageVisits.objects.first()
+    if not page_visits:
+        page_visits = PageVisits.objects.create(count=1)
+    else:
+        page_visits.count += 1
+        page_visits.save()
+    return HttpResponse('Hello World! You are visitor no. {0}.'.format(page_visits.count))
